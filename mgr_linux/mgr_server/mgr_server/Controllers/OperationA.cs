4ca78432239da5075d4c9a0f3f0b6ea77d8e89d4
ksazon <krzysztof.sazon@gmail.com>
2020-06-17 14:46:21 +0200

    add server

---
+++ mgr_linux/mgr_server/mgr_server/Controllers/OperationA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace mgr_server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OperationA : ControllerBase
+    {
+        private readonly ILogger<OperationA> _logger;
+
+        public OperationA(ILogger<OperationA> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{size}")]
+        public int Get(int size)
+        {
+            Thread.Sleep(size);
+            return (new Random()).Next();
+        }
+    }
+}
