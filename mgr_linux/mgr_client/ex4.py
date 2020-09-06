86c9ee9357383d3a3272890e979eb7a6302f0db6
ksazon <krzysztof.sazon@gmail.com>
2020-09-07 01:49:43 +0200

    sth

--- mgr_linux/mgr_client/ex4.py
+++ mgr_linux/mgr_client/ex4.py
@@ -28,7 +28,7 @@ def alg_random(table):
 def alg_insertion_beam(table: pd.DataFrame, conflits: pd.DataFrame):
     t_table = table.to_numpy()
     t_conflits = conflits.to_numpy()
-    rank_matrix = np.full(t_table.shape, np.nan)
+    # rank_matrix = np.full(t_table.shape, np.nan)
     candidate_schedules = [np.full(t_table.shape, np.nan),]
 
     def solve_conflicts(rm: np.ndarray, changed_rows: Set[int], changed_cols: Set[int]):
@@ -37,8 +37,8 @@ def alg_insertion_beam(table: pd.DataFrame, conflits: pd.DataFrame):
 
         for cr in changed_rows:
             for cc in changed_cols:
-                for cv in np.where(rm[cr,:] == rm[cr,cc]):
-                    if cv[0] == cc:
+                for cv in np.where(rm[:,cc] == rm[cr,cc])[0]:
+                    if cv == cr:
                         continue
                     new_changed_rows.add(cr)
                     new_changed_cols.add(cc)
@@ -49,9 +49,9 @@ def alg_insertion_beam(table: pd.DataFrame, conflits: pd.DataFrame):
         
         return rm
 
-    def conflicts(row, col):
-        if col == 2 and not np.isnan(rank_matrix[row,3]): return [(row,3),]
-        if col == 3 and not np.isnan(rank_matrix[row,2]): return [(row,2),]
+    def row_conflicts(rm: np.ndarray, row: int, col: int):
+        if col == 2 and not np.isnan(rm[row,3]): return [(row,3),]
+        if col == 3 and not np.isnan(rm[row,2]): return [(row,2),]
         return []
 
     def insertion_order():
@@ -60,26 +60,27 @@ def alg_insertion_beam(table: pd.DataFrame, conflits: pd.DataFrame):
 
     def beam_search(rm: Set[np.ndarray]):
         # TODO better function
-        return random.sample(rm, min(len(rm), 2))
+        return random.sample(rm, min(len(rm), 5))
 
     for row, col in insertion_order():
+        candidate_schedules_with_children = []
         for cs in candidate_schedules:
-        potential_rank = set()
-        children = []
+            potential_rank = set()
+            # children = []
 
-        potential_rank.add(1)
-        for e in np.argwhere(~np.isnan(rank_matrix[:,col])):
-            potential_rank.add(rank_matrix[e,col]+1)
-        for cell in conflicts(row, col):
-            potential_rank.add(rank_matrix[cell]+1)
+            potential_rank.add(1)
+            for e in np.argwhere(~np.isnan(cs[:,col])):
+                potential_rank.add(cs[(e,col)][0]+1)
+            for cell in row_conflicts(cs, row, col):
+                potential_rank.add(cs[cell]+1)
 
-        for pr in potential_rank:
-            prm = cs.copy()
-            prm[row, col] = pr
-            prm = solve_conflicts(prm, {row,}, {col,})
-            potential_rank_matrices.append(prm)
+            for pr in potential_rank:
+                cs_with_child = cs.copy()
+                cs_with_child[row, col] = pr
+                cs_with_child = solve_conflicts(cs_with_child, {row,}, {col,})
+                candidate_schedules_with_children.append(cs_with_child)
         
-        potential_rank_matrices_truncated = beam_search(potential_rank_matrices)
+        candidate_schedules = beam_search(candidate_schedules_with_children)
         
 
 
